f513b5acfda7a584c9f0f983418c7f163fc9f7e0
marwamprog-js <marwamprog.js@gmail.com>
2023-09-21 15:41:48 -0300

    Commit inicial projeto mentoria Java

---
+++ src/main/java/com/maltadev/mentoria/lojavirtual/SpringLojaVirtualMentoriaApplication.java
@@ -0,0 +1,13 @@
+package com.maltadev.mentoria.lojavirtual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLojaVirtualMentoriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLojaVirtualMentoriaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maltadev/mentoria/lojavirtual/SpringLojaVirtualMentoriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maltadev.mentoria.lojavirtual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLojaVirtualMentoriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
