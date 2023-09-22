016362ceeb2be5a5e68640f13bec9a394d1747c2
marwamprog-js <marwamprog.js@gmail.com>
2023-09-22 11:28:13 -0300

    Aula Mentoria - 22/09/2023
    * Criação das classes
    	ContaPagar
    	ContaReceber
    	FormaPagamento
    
    * Criação dos Enums
    	StatusContaPagar
    	StatusContaReceber
    
    * Versionamento do banco
    	pacote db.migration
    	Exclusão da constrante criada 		automaticamente pela JPA da tabela
    usuarios_acesso

---
+++ src/main/java/com/maltadev/mentoria/lojavirtual/model/ContaPagar.java
@@ -0,0 +1,165 @@
+package com.maltadev.mentoria.lojavirtual.model;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.ConstraintMode;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.ForeignKey;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import com.maltadev.mentoria.lojavirtual.enums.StatusContaPagar;
+
+@Entity
+@Table(name = "conta_pagar")
+@SequenceGenerator(name = "seq_conta_pagar", sequenceName = "seq_conta_pagar", allocationSize = 1, initialValue = 1)
+public class ContaPagar implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_conta_pagar")
+	private Long id;
+	
+	@Column(name = "descricao")
+	private String descricao;
+	
+	@Column(name = "valor_total")
+	private BigDecimal valorTotal;
+	
+	@Column(name = "valor_desconto")
+	private BigDecimal valorDesconto;
+	
+	@Temporal(TemporalType.DATE)
+	private Date dtVencimento;
+	
+	@Temporal(TemporalType.DATE)
+	private Date dtPagamento;
+
+	@Enumerated(EnumType.STRING)
+	private StatusContaPagar status;
+	
+	/**
+	 * Eu sou a pessoa que tem que pagar
+	 */
+	@ManyToOne(targetEntity = Pessoa.class)
+	@JoinColumn(name = "pessoa_id", nullable = false, foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "pessoa_fk"))
+	private Pessoa pessoa;
+	
+	/**
+	 * Para quem tenho que pagar
+	 */
+	@ManyToOne(targetEntity = Pessoa.class)
+	@JoinColumn(name = "pessoa_forn_fk", nullable = false, foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "pessoa_forn_fk"))
+	private Pessoa pessoaFornecedor;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public BigDecimal getValorTotal() {
+		return valorTotal;
+	}
+
+	public void setValorTotal(BigDecimal valorTotal) {
+		this.valorTotal = valorTotal;
+	}
+
+	public BigDecimal getValorDesconto() {
+		return valorDesconto;
+	}
+
+	public void setValorDesconto(BigDecimal valorDesconto) {
+		this.valorDesconto = valorDesconto;
+	}
+
+	public Date getDtVencimento() {
+		return dtVencimento;
+	}
+
+	public void setDtVencimento(Date dtVencimento) {
+		this.dtVencimento = dtVencimento;
+	}
+
+	public Date getDtPagamento() {
+		return dtPagamento;
+	}
+
+	public void setDtPagamento(Date dtPagamento) {
+		this.dtPagamento = dtPagamento;
+	}
+
+	public StatusContaPagar getStatus() {
+		return status;
+	}
+
+	public void setStatus(StatusContaPagar status) {
+		this.status = status;
+	}
+
+	public Pessoa getPessoa() {
+		return pessoa;
+	}
+
+	public void setPessoa(Pessoa pessoa) {
+		this.pessoa = pessoa;
+	}
+
+	public Pessoa getPessoaFornecedor() {
+		return pessoaFornecedor;
+	}
+
+	public void setPessoaFornecedor(Pessoa pessoaFornecedor) {
+		this.pessoaFornecedor = pessoaFornecedor;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ContaPagar other = (ContaPagar) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	@Override
+	public String toString() {
+		return "ContaPagar [id=" + id + "]";
+	}
+
+	
+	
+}
--- src/main/java/com/maltadev/mentoria/lojavirtual/model/Usuario.java
+++ src/main/java/com/maltadev/mentoria/lojavirtual/model/Usuario.java
@@ -45,12 +45,16 @@ public class Usuario implements UserDetails {
 	private Date dataAtualSenha;
 	
 	@OneToMany(fetch = FetchType.LAZY)
-	@JoinTable(name = "usuarios_acesso", uniqueConstraints = @UniqueConstraint(columnNames = {"usuario_id", "acesso_id"},
+	@JoinTable(name = "usuarios_acesso", 
+	uniqueConstraints = @UniqueConstraint(columnNames = {"usuario_id", "acesso_id"},
 	name = "unique_acesso_user"),
+	
 	joinColumns = @JoinColumn(name = "usuario_id", referencedColumnName = "id", table = "usuario", 
 	unique = false, foreignKey = @ForeignKey(name = "usuario_fk", value = ConstraintMode.CONSTRAINT)), 
-	inverseJoinColumns = @JoinColumn(name = "acesso_id", referencedColumnName = "id", table = "acesso", 
-	unique = false, foreignKey = @ForeignKey(name = "acesso_fk", value = ConstraintMode.CONSTRAINT)))
+	
+	inverseJoinColumns = @JoinColumn(name = "acesso_id", 
+		referencedColumnName = "id", table = "acesso", 
+		unique = false, foreignKey = @ForeignKey(name = "acesso_fk", value = ConstraintMode.CONSTRAINT)))
 	private List<Acesso> acessos;
 	
 	
