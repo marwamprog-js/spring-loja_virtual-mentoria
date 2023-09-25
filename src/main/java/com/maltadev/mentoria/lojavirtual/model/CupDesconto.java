aa7834f79289fc4749366114fda04f22cbc17fcd
marwamprog-js <marwamprog.js@gmail.com>
2023-09-25 11:27:25 -0300

    Aula mentoria - 25/09/2023
    Adição das classes
     CupDesconto
     ImagemProduto
     NotaFiscalCompra
     NotaItemProduto
     Produto

---
+++ src/main/java/com/maltadev/mentoria/lojavirtual/model/CupDesconto.java
@@ -0,0 +1,104 @@
+package com.maltadev.mentoria.lojavirtual.model;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name = "cup_desconto")
+@SequenceGenerator(name = "seq_cup_desconto", sequenceName = "seq_cup_desconto", allocationSize = 1, initialValue = 1)
+public class CupDesconto implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_cup_desconto")
+	private Long id;
+	
+	@Column(name = "cod_desc", nullable = false)
+	private String codDesc;
+	
+	@Column(name = "valor_real_desc", nullable = false)
+	private BigDecimal valorRealDesc;
+	
+	@Column(name = "valor_porcent_desc", nullable = false)
+	private BigDecimal valorPorcentDesc;
+	
+	@Temporal(TemporalType.DATE)
+	@Column(name = "data_validade_cupom")
+	private Date dataValidadeCupom;
+	
+	
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getCodDesc() {
+		return codDesc;
+	}
+
+	public void setCodDesc(String codDesc) {
+		this.codDesc = codDesc;
+	}
+
+	public BigDecimal getValorRealDesc() {
+		return valorRealDesc;
+	}
+
+	public void setValorRealDesc(BigDecimal valorRealDesc) {
+		this.valorRealDesc = valorRealDesc;
+	}
+
+	public BigDecimal getValorPorcentDesc() {
+		return valorPorcentDesc;
+	}
+
+	public void setValorPorcentDesc(BigDecimal valorPorcentDesc) {
+		this.valorPorcentDesc = valorPorcentDesc;
+	}
+
+	public Date getDataValidadeCupom() {
+		return dataValidadeCupom;
+	}
+
+	public void setDataValidadeCupom(Date dataValidadeCupom) {
+		this.dataValidadeCupom = dataValidadeCupom;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		CupDesconto other = (CupDesconto) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	@Override
+	public String toString() {
+		return "Endereco [id=" + id + "]";
+	}
+	
+	
+	
+	
+}
